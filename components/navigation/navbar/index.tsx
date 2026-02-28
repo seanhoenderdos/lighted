d17185099fe22de32f023eced67232da33d64e2a
seanhoenderdos <101661395+seanhoenderdos@users.noreply.github.com>
2026-02-28 20:29:12 +0200

    Replace logo with Lighted text in navbar

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Theme from "./Theme";
 import { useSession, signOut } from "next-auth/react";
+import Image from "next/image";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -24,13 +24,8 @@ const Navbar = () => {
 
   return (
     <nav className="flex justify-between fixed z-50 w-full p-6 dark:shadow-none sm:px-12 border border-border-line gap-5 backdrop-blur-xl">
-      <Link href="/" className="flex items-center gap-1">
-        <Image
-          src="/images/site-logo.svg"
-          width={125}
-          height={125}
-          alt="lighted Logo"
-        />
+      <Link href="/" className="flex items-center gap-2">
+        <span className="text-2xl font-bold">Lighted</span>
       </Link>
 
       <div className="flex items-center gap-4">
